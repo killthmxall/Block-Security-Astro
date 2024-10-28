8a4fde9593623b479b173c4c5c28f24b9b62d1e1
Erick <killthmxall@outlook.com>
2024-10-27 23:03:41 -0500

    Se corrigen los errores de repetición y se crea un layout

--- src/pages/intro-app.js
+++ src/pages/intro-app.js
@@ -11,12 +11,12 @@ window.addEventListener('DOMContentLoaded', ()=> {
         });
 
         setTimeout(() => {
-           logoSpan.forEach((span, idx) =>{
+            logoSpan.forEach((span, idx) =>{
                 setTimeout(() =>{
                     span.classList.remove('active');
                     span.classList.add('fade');
                 }, (idx + 1) * 50)
-           })
+            })
         }, 2000);
 
         setTimeout(()=>{
